92a8fe4ae9a4ce8953fee3520e4b1a0cc9c2dce2
Luke <luke14dennis@gmail.com>
2026-03-26 18:46:57 +0200

    Add debug mode to Flask app and include YOLO model file

--- ai_backend/app.py
+++ ai_backend/app.py
@@ -45,4 +45,4 @@ def detect_objects():
     return jsonify({'detections': detections})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
